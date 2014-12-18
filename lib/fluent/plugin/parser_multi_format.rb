56313aef080155c46aa8f47a143d9162c5af9729
Masahiro Nakagawa <repeatedly@gmail.com>
2014-12-18 19:31:16 +0900

    Support new Parser API

--- lib/fluent/plugin/parser_multi_format.rb
+++ lib/fluent/plugin/parser_multi_format.rb
@@ -2,52 +2,96 @@ require 'fluent/parser'
 
 module Fluent
   class TextParser
-    class MultiFormatParser
-      include Configurable
+    if defined?(::Fluent::Parser)
+      class MultiFormatParser < Parser
+        Plugin.register_parser('multi_format', self)
 
-      def initialize
-        super
+        def initialize
+          super
 
-        @parsers = []
-      end
+          @parsers = []
+        end
 
-      def configure(conf)
-        super
+        def configure(conf)
+          super
 
-        conf.elements.each { |e|
-          next unless ['pattern', 'format'].include?(e.name)
+          conf.elements.each { |e|
+            next unless ['pattern', 'format'].include?(e.name)
 
-          parser = TextParser.new
-          parser.configure(e)
-          @parsers << parser.parser
-        }
-      end
+            parser = Plugin.new_parser(e['format'])
+            parser.configure(e)
+            @parsers << parser
+          }
+        end
 
-      def call(text)
-        @parsers.each { |parser|
-          begin
-            parser.call(text) { |time, record|
-              if time && record
-                if block_given?
+        def parse(text)
+          @parsers.each { |parser|
+            begin
+              parser.parse(text) { |time, record|
+                if time && record
                   yield time, record
                   return
-                else
-                  return time, record
                 end
-              end
-            }
-          rescue # ignore parser error
-          end
-        }
+              }
+            rescue # ignore parser error
+            end
+          }
 
-        if block_given?
           yield nil, nil
-        else
-          return nil, nil
         end
       end
-    end
+    else # support old API. Will be removed.
+      class MultiFormatParser
+        include Configurable
 
-    register_template('multi_format', Proc.new { MultiFormatParser.new })
+        def initialize
+          super
+
+          @parsers = []
+        end
+
+        def configure(conf)
+          super
+
+          conf.elements.each { |e|
+            next unless ['pattern', 'format'].include?(e.name)
+
+            parser = TextParser.new
+            parser.configure(e)
+            @parsers << parser.parser
+          }
+        end
+
+        def parse(text)
+          @parsers.each { |parser|
+            begin
+              parser.call(text) { |time, record|
+                if time && record
+                  if block_given?
+                    yield time, record
+                    return
+                  else
+                    return time, record
+                  end
+                end
+              }
+            rescue # ignore parser error
+            end
+          }
+
+          if block_given?
+            yield nil, nil
+          else
+            return nil, nil
+          end
+        end
+
+        def call(*a, &b)
+          parse(*a, &b)
+        end
+      end
+
+      register_template('multi_format', Proc.new { MultiFormatParser.new })
+    end
   end
 end
