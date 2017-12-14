721f3d265e5418ebd78ebee3ba32f899e8c4e139
Masahiro Nakagawa <repeatedly@gmail.com>
2017-12-15 04:35:21 +0900

    Use new API

--- lib/fluent/plugin/parser_multi_format.rb
+++ lib/fluent/plugin/parser_multi_format.rb
@@ -1,98 +1,44 @@
-require 'fluent/parser'
+require 'fluent/plugin/parser'
 
 module Fluent
-  class TextParser
-    if defined?(::Fluent::Parser)
-      class MultiFormatParser < Parser
-        Plugin.register_parser('multi_format', self)
+  module Plugin
+    class MultiFormatParser < Parser
+      Plugin.register_parser('multi_format', self)
 
-        def initialize
-          super
+      def initialize
+        super
 
-          @parsers = []
-        end
-
-        def configure(conf)
-          super
-
-          conf.elements.each { |e|
-            next unless ['pattern', 'format'].include?(e.name)
-            next if e['format'].nil? && (e['@type'] == 'multi_format')
-
-            parser = Plugin.new_parser(e['format'])
-            parser.configure(e)
-            @parsers << parser
-          }
-        end
-
-        def parse(text)
-          @parsers.each { |parser|
-            begin
-              parser.parse(text) { |time, record|
-                if time && record
-                  yield time, record
-                  return
-                end
-              }
-            rescue # ignore parser error
-            end
-          }
-
-          yield nil, nil
-        end
+        @parsers = []
       end
-    else # support old API. Will be removed.
-      class MultiFormatParser
-        include Configurable
 
-        def initialize
-          super
+      def configure(conf)
+        super
 
-          @parsers = []
-        end
+        conf.elements.each { |e|
+          next unless ['pattern', 'format'].include?(e.name)
+          next if e['format'].nil? && (e['@type'] == 'multi_format')
 
-        def configure(conf)
-          super
+          parser = Plugin.new_parser(e['format'])
+          parser.configure(e)
+          @parsers << parser
+        }
+      end
 
-          conf.elements.each { |e|
-            next unless ['pattern', 'format'].include?(e.name)
-
-            parser = TextParser.new
-            parser.configure(e)
-            @parsers << parser.parser
-          }
-        end
-
-        def parse(text)
-          @parsers.each { |parser|
-            begin
-              parser.call(text) { |time, record|
-                if time && record
-                  if block_given?
-                    yield time, record
-                    return
-                  else
-                    return time, record
-                  end
-                end
-              }
-            rescue # ignore parser error
-            end
-          }
-
-          if block_given?
-            yield nil, nil
-          else
-            return nil, nil
+      def parse(text)
+        @parsers.each { |parser|
+          begin
+            parser.parse(text) { |time, record|
+              if time && record
+                yield time, record
+                return
+              end
+            }
+          rescue # ignore parser error
           end
-        end
+        }
 
-        def call(*a, &b)
-          parse(*a, &b)
-        end
+        yield nil, nil
       end
-
-      register_template('multi_format', Proc.new { MultiFormatParser.new })
     end
   end
 end
